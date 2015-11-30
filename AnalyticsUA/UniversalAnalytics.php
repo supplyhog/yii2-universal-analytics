f8e7804f1eb3f2dac0df97773c1e65cda1ebfade
Wil Wade <wil@supplyhog.com>
2015-11-30 11:23:47 -0500

    changes to the ec calls

--- AnalyticsUA/UniversalAnalytics.php
+++ AnalyticsUA/UniversalAnalytics.php
@@ -1,51 +1,51 @@
 <?php
+/**
+ * Notes:
+ * Use ec_ to prefix any Enhanced Ecommerce calls
+ */
+namespace supplyhog\AnalyticsUA;
+
+use Yii;
+use yii\base\Component;
+use yii\helpers\Json;
+
+class UniversalAnalytics extends Component
+{
 	/**
-	 * Notes:
-	 * Use ec_ to prefix any Enhanced Ecommerce calls
+	 * Property ID
+	 * @var string
 	 */
-	namespace supplyhog\AnalyticsUA;
+	public $property = '';
 
-	use Yii;
-	use yii\base\Component;
-	use yii\helpers\Json;
+	/**
+	 * Debug
+	 * @var bool
+	 */
+	public $debug = false;
 
-	class UniversalAnalytics extends Component
-	{
-		/**
-		 * Property ID
-		 * @var string
-		 */
-		public $property = '';
+	/**
+	 * Auto Render
+	 * @var bool
+	 */
+	public $autoRender = false;
 
-		/**
-		 * Debug
-		 * @var bool
-		 */
-		public $debug = false;
+	/**
+	 * Automatically add trackPageview when render is called
+	 * @var bool
+	 */
+	public $autoPageview = true;
 
-		/**
-		 * Auto Render
-		 * @var bool
-		 */
-		public $autoRender = false;
+	/**
+	 * Enable Enhanced Ecommerce plugin
+	 * @var bool
+	 */
+	public $enhancedEcommerce = false;
 
-		/**
-		 * Automatically add trackPageview when render is called
-		 * @var bool
-		 */
-		public $autoPageview = true;
-
-		/**
-		 * Enable Enhanced Ecommerce plugin
-		 * @var bool
-		 */
-		public $enhancedEcommerce = false;
-
-		/**
-		 * Allowable Settings
-		 * @protected
-		 */
-		protected $_settings = [
+	/**
+	 * Allowable Settings
+	 * @protected
+	 */
+	protected $_settings = [
 			'alwaysSendReferrer',
 			'allowAnchor',
 			'clientId',
@@ -56,48 +56,54 @@
 			'name',
 			'sampleRate',
 			'siteSpeedSampleRate',
-		];
+	];
 
-		/**
-		 * Settings Data
-		 * @protected
-		 */
-		protected $_settingsData = [];
+	/**
+	 * Settings Data
+	 * @protected
+	 */
+	protected $_settingsData = [];
 
-		/**
-		 * Called data
-		 * @protected
-		 */
-		protected $_calledData = [];
+	/**
+	 * Called data
+	 * @protected
+	 */
+	protected $_calledData = [];
 
-		/**
-		 * Have we rendered already?
-		 * @protected
-		 */
-		protected $_hasRendered = false;
+	/**
+	 * Ecommerce Data
+	 * @protected
+	 */
+	protected $_ecData = [];
 
-		/**
-		 * Render JS
-		 * @return mixed
-		 */
-		public function render()
-		{
-			$js = '';
-			if (!$this->_hasRendered) {
-				if ($this->property == '') {
-					$this->_debug('No property ID for Universal Analytics - cannot render', 'error');
-					return;
-				}
+	/**
+	 * Have we rendered already?
+	 * @protected
+	 */
+	protected $_hasRendered = false;
 
-				// Check to see if we need to throw in the trackPageview call
-				if ($this->autoPageview && !in_array('_trackPageview', $this->_calledData)) {
-					$this->send('pageview');
-				}
+	/**
+	 * Render JS
+	 * @return mixed
+	 */
+	public function render()
+	{
+		$js = '';
+		if (!$this->_hasRendered) {
+			if ($this->property == '') {
+				$this->_debug('No property ID for Universal Analytics - cannot render', 'error');
+				return;
+			}
 
-				// Setup code block
-				$fileName = ($this->debug) ? 'analytics_debug.js' : 'analytics.js';
+			// Check to see if we need to throw in the trackPageview call
+			if ($this->autoPageview && !in_array('_trackPageview', $this->_calledData)) {
+				$this->send('pageview');
+			}
 
-				$js .= <<<EOT
+			// Setup code block
+			$fileName = ($this->debug) ? 'analytics_debug.js' : 'analytics.js';
+
+			$js .= <<<EOT
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -105,138 +111,173 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/{$fileName}','ga');
 
 EOT;
-				// Do we have data for settings?
-				if (count($this->_settingsData)) {
-					$js .= "ga('create', '{$this->property}', ";
-					$js .= Json::encode($this->_settingsData);
-					$js .= ");" . PHP_EOL;
-				} else {
-					$js .= "ga('create', '{$this->property}');" . PHP_EOL;
-				}
-
-				if($this->enhancedEcommerce) {
-					$js .= "ga('require', 'ec');" . PHP_EOL;
-				}
-
-				// Append a period if we have an identifier (name)
-				$this->name = ($this->name) ? $this->name . '.' : '';
-
-				$this->_hasRendered = true;
+			// Do we have data for settings?
+			if (count($this->_settingsData)) {
+				$js .= "ga('create', '{$this->property}', ";
+				$js .= Json::encode($this->_settingsData);
+				$js .= ");" . PHP_EOL;
+			} else {
+				$js .= "ga('create', '{$this->property}');" . PHP_EOL;
 			}
 
-			foreach ($this->_calledData as $call) {
-				$js .= "ga('{$this->name}{$call['type']}'";
-				$tempData = [];
-				if (count($call['data'])) {
-					foreach ($call['data'] as $data) {
-						// What to do with the types?
-						switch (gettype($data)) {
-							case 'array':
-							case 'object':
-								$tempData[] = Json::encode($data);
-								break;
-							case 'integer':
-							case 'double':
-								$tempData[] = $data;
-								break;
-							case 'boolean':
-							case 'null':
-								$tempData[] = ($data) ? 'true' : 'false';
-								break;
-							case 'string':
-							default:
-								$tempData[] = "'" . $data . "'";
-								break;
-						}
+			if($this->enhancedEcommerce) {
+				$js .= "ga('require', 'ec');" . PHP_EOL;
+				foreach($this->_ecData as $call) {
+					$js .= "ga('{$call['call']}'";
+					$js .= ', ' . $this->toJs($call['data']);
+					if(isset($call['secondary'])) {
+						$js .= ', ' . $this->toJs($call['secondary']);
 					}
-					$js .= ', ' . implode(", ", $tempData);
+					$js .= ');' . PHP_EOL;
 				}
-				$js .= ")" . PHP_EOL;
 			}
 
-			// Clear our the current data, so we can continue to render new items
-			// .. and not repeat ourselves!
-			$this->_calledData = [];
+			// Append a period if we have an identifier (name)
+			$this->name = ($this->name) ? $this->name . '.' : '';
 
-			if ($this->autoRender) {
-				$this->_debug('Autorender enabled, adding Universal Analytics via clientScript', 'info');
-				//@todo figure out how to register the script from the component
-				Yii::app()->clientScript
-					->registerScript('TPUniversalAnalytics', $js, CClientScript::POS_HEAD);
-				return;
-			}
-
-			return $js;
+			$this->_hasRendered = true;
 		}
 
-		/**
-		 * Call a UA method
-		 * @param string $method
-		 * @param mixed  $args
-		 * @return bool
-		 */
-		public function __call($method, $args)
-		{
-			// Stupid ecommerce plugin, messing things up
-			if(substr($method, 3) === 'ec_') {
-				$method = str_replace('_', ':', $method);
+		foreach ($this->_calledData as $call) {
+			$js .= "ga('{$call['type']}'";
+			$tempData = [];
+			if (count($call['data'])) {
+				foreach ($call['data'] as $data) {
+					// What to do with the types?
+					$tempData[] = $this->toJs($data);
+				}
+				$js .= ', ' . implode(", ", $tempData);
 			}
-			switch ($method) {
-				case 'send':
-				case 'set':
-				case 'require':
-					$this->_calledData[] = [
+			$js .= ")" . PHP_EOL;
+		}
+
+		// Clear our the current data, so we can continue to render new items
+		// .. and not repeat ourselves!
+		$this->_calledData = [];
+
+		if ($this->autoRender) {
+			$this->_debug('Autorender enabled, adding Universal Analytics via clientScript', 'info');
+			//@todo figure out how to register the script from the component
+			Yii::app()->clientScript
+					->registerScript('TPUniversalAnalytics', $js, CClientScript::POS_HEAD);
+			return;
+		}
+
+		return $js;
+	}
+
+	/**
+	 * Call an Enhanced Ecommerce method
+	 *
+	 * @param string $event addProduct, setAction, etc...
+	 * @param mixed $data The first piece of data, generally an array
+	 * @param array $additionalData The second optional array param on some ec calls.
+	 * @return $this;
+	 */
+	public function ec($event, $data, $additionalData = [])
+	{
+		$data = [
+				'call' => 'ec:' . $event,
+				'data' => $data,
+		];
+		if($additionalData) {
+			$data['secondary'] = $additionalData;
+		}
+		$this->_ecData[] = $data;
+		return $this;
+	}
+
+	/**
+	 * Call a UA method
+	 * @param string $method
+	 * @param mixed  $args
+	 * @return bool
+	 */
+	public function __call($method, $args)
+	{
+		// Stupid ecommerce plugin, messing things up
+		if(substr($method, 3) === 'ec_') {
+			$method = str_replace('_', ':', $method);
+		}
+		switch ($method) {
+			case 'send':
+			case 'set':
+			case 'require':
+				$this->_calledData[] = [
 						'type' => $method,
 						'data' => $args,
-					];
-					return true;
-					break;
-				default:
-					// No default, for now
-					$this->_debug('Invalid Universal Analytics method call: ' . $method, 'warning');
-					return false;
-					break;
-			}
-		}
-
-		/**
-		 * Set a property (UA setting)
-		 * @param string $property
-		 * @param mixed  $value
-		 * @return mixed
-		 */
-		public function __set($property, $value)
-		{
-			if (in_array($property, $this->_settings) && $value !== null && $value != '') {
-				$this->_settingsData[$property] = $value;
-				return;
-			}
-		}
-
-		/**
-		 * Get a property (UA setting)
-		 * @param string $property
-		 * @return mixed
-		 */
-		public function __get($property)
-		{
-			if (isset($this->_settingsData[$property]))
-				return $this->_settingsData[$property];
-
-			return null;
-		}
-
-		/**
-		 * Output debugging, if enabled
-		 * @param string $message
-		 * @param string $level
-		 * @protected
-		 */
-		protected function _debug($message = '', $level = 'info')
-		{
-			if ($this->debug) {
-				Yii::getLogger()->log($message, $level, 'AnalyticsUA.UniversalAnalytics');
-
-			}
+				];
+				return true;
+				break;
+			default:
+				// No default, for now
+				$this->_debug('Invalid Universal Analytics method call: ' . $method, 'warning');
+				return false;
+				break;
 		}
 	}
+
+	/**
+	 * Takes the data and readies for js
+	 * @param mixed $data
+	 * @return string
+	 */
+	public function toJs($data)
+	{
+		switch (gettype($data)) {
+			case 'array':
+			case 'object':
+				return Json::encode($data);
+			case 'integer':
+			case 'double':
+				return $data;
+			case 'boolean':
+			case 'null':
+				return ($data) ? 'true' : 'false';
+			case 'string':
+			default:
+				return "'" . $data . "'";
+		}
+	}
+
+	/**
+	 * Set a property (UA setting)
+	 * @param string $property
+	 * @param mixed  $value
+	 * @return mixed
+	 */
+	public function __set($property, $value)
+	{
+		if (in_array($property, $this->_settings) && $value !== null && $value != '') {
+			$this->_settingsData[$property] = $value;
+			return;
+		}
+	}
+
+	/**
+	 * Get a property (UA setting)
+	 * @param string $property
+	 * @return mixed
+	 */
+	public function __get($property)
+	{
+		if (isset($this->_settingsData[$property]))
+			return $this->_settingsData[$property];
+
+		return null;
+	}
+
+	/**
+	 * Output debugging, if enabled
+	 * @param string $message
+	 * @param string $level
+	 * @protected
+	 */
+	protected function _debug($message = '', $level = 'info')
+	{
+		if ($this->debug) {
+			Yii::getLogger()->log($message, $level, 'AnalyticsUA.UniversalAnalytics');
+
+		}
+	}
+}
